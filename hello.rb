7926c68bc46d9e7d48f364ad6f36cfaa50fecc43
Spike Nox <spike01@gmail.com>
2014-09-02 18:27:47 +0100

    Backing up to git

---
+++ hello.rb
@@ -0,0 +1,15 @@
+require 'sinatra'
+
+get '/' do
+  @name = %w(Amigo Oscar Viking).sample
+  erb :index
+end
+
+get '/secret' do
+  'This is a secret page'
+end
+
+get '/hello' do
+  @visitor = params[:name]
+  erb :index
+end
